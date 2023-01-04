f467694b7755cfeb9a0a00650da5edfddb63c49d
lekosk2001 <lekosk2001@naver.com>
2023-01-04 18:25:21 +0900

    초기세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const onSubmit = () =>{
+    console.log(title,body)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='mb-3 mt-3'>
+        <label className='form-lable'>Title</label>
+        <input 
+          className='form-control'
+          value={title}
+          onChange={(e)=>{setTitle(e.target.value)}}
+        />
+
+        <label className='form-lable'>Body</label>
+        <textarea 
+          className='form-control'
+          rows="20"
+          value={body}
+          onChange={(e)=>{setBody(e.target.value)}}
+        />
+      </div>
+      <button className='btn btn-primary' onClick={onSubmit}>Post</button>
     </div>
   );
 }
