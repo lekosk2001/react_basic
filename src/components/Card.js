7834b473ad63708b9c6202c5d8f44da92519ff99
lekosk2001 <lekosk2001@naver.com>
2023-01-07 19:18:50 +0900

    feat : 블로그 목록 카드 스타일.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,29 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
-export default function Card(props) {
+export default function Card({title,children,onClick}) {
     return (
-        <div className='card mb-3' key={props.id}>
-            <div className='card-body'>{props.title}</div>
+        <div 
+            className='card mb-3 list-group-item-action pe-auto'
+            onClick={onClick}
+        >
+            <div className='card-body'>
+                <div className='d-flex justify-content-between'>
+                    <div className='card-title'>{title}</div>
+                    {children&&<>{children}</>}
+                </div>
+            </div>
         </div>
     )
+}
+
+Card.propTypes ={
+    title:PropTypes.string.isRequired,
+    children:PropTypes.element,
+    onClick:PropTypes.func
+}
+
+Card.defaultProps = {
+    children:null,
+    onClick:()=>{}
 }
--- src/pages/BlogListsPage.js
+++ src/pages/BlogListsPage.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import React, { useState,useEffect } from 'react'
+import { Link, redirect,useNavigate  } from 'react-router-dom'
 import Card from '../components/Card'
 
 export default function BlogListsPage() {
 const [lists, setLists] = useState([])
+const navigate = useNavigate()
 
   const getPosts = ()=>{
     axios.get('http://localhost:3001/posts').then((req)=>{
@@ -16,9 +18,21 @@ const [lists, setLists] = useState([])
   }, [])
   
   return (
-    <div>
-      <h1>Blogs</h1>
-      {lists.map(post=>{return <Card id={post.id} title={post.title}></Card>})}
+    <div className='mt-3 list-group'>
+      <div className='d-flex justify-content-between'>
+        <h1>Blogs</h1>
+        <div>
+          <Link to='/blogs/create' className='btn btn-success'>Create New</Link>
+        </div>
+      </div>
+
+      {lists.map(post=>{return <Card 
+        key={post.id}
+        title={post.title}
+        onClick={()=>{navigate("/blogs/edit")}}
+        >
+        <button>button</button>
+      </Card>})}
     </div>
   )
 }
