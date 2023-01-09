1ca6d765ba4e0d6270ecc39c6fa06993e1d3cc06
lekosk2001 <lekosk2001@naver.com>
2023-01-09 19:06:47 +0900

    feat : 퍼블리쉬 체크박스 구분 구현.

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -5,22 +5,45 @@ import { useNavigate, useParams  } from 'react-router-dom'
 function BlogForm({editing}) {
 
 const navigate = useNavigate()
+
+const [originalTitle, setOriginalTitle] = useState('')
 const [title, setTitle] = useState('')
+const [originalBody, setOriginalBody] = useState('')
 const [body, setBody] = useState('')
+const [originalPublish, setOriginalPublish] = useState(false)
+const [publish, setPublish] = useState(false)
+
+const isEdited = () => {
+    return title !== originalTitle || body !== originalBody || publish !== originalPublish
+}
+
+const onChangePublish = (e) => {
+    setPublish(e.target.checked)
+}
 
 const onSubmit = () =>{
     if(editing){
         axios.patch(`http://localhost:3001/posts/${id}`,{
             title,
             body,
-            createdAt:Date.now()
-        }).then(navigate("/blogs"))
+            publish,
+        }).then(navigate(`/blogs/${id}`))
     }else{
         axios.post('http://localhost:3001/posts',{
             title,
             body,
+            publish,
             createdAt:Date.now()
         }).then(navigate("/blogs"))
+    }
+}
+
+const goBack = () => {
+    if(editing){
+        navigate(`/blogs/${id}`)
+    }
+    else{
+        navigate(`/blogs`)
     }
 }
 
@@ -31,8 +54,12 @@ useEffect(() => {
         axios.get(`http://localhost:3001/posts/${id}`).then((res)=>{
         setTitle(res.data.title);
         setBody(res.data.body);
+        setPublish(res.data.publish);
+        setOriginalTitle(res.data.title);
+        setOriginalBody(res.data.body);
+        setOriginalPublish(res.data.publish);
     })}
-}, [id])
+}, [id,editing])
 
 
 return (
@@ -54,12 +81,35 @@ return (
         onChange={(e)=>{setBody(e.target.value)}}
     />
     </div>
+
+    <div className='form-check mb-3'>
+        <input
+                className='form-check-input'
+                type="checkbox"
+                checked={publish}
+                onChange={onChangePublish}
+                id="publishCheck"
+
+            /> 
+                <label htmlFor="publishCheck" className='form-check-label'>Publish</label>
+
+    </div>
+
     <button
         className='btn btn-primary'
         onClick={onSubmit}
+        disabled={editing && !isEdited()}
     >
         {editing?"Edit":"Post"}
     </button>
+
+    <button
+        className='btn btn-danger ms-2'
+        onClick={goBack}
+    >
+        Cancle
+    </button>
+
 </div>
 );
 }
